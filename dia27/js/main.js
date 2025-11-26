440f1e7faeb09068e987169f5a33e5880a7cb336
AndreaDom3 <andreadominguez136@gmail.com>
2025-11-26 18:57:53 +0100

    dia 27

---
+++ dia27/js/main.js
@@ -0,0 +1,17 @@
+//NIVEL 1
+let semana = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo", "festivo", "laboral", "nacional"];
+
+const miArray = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo", "festivo", "laboral", "nacional"];
+
+miArray.splice(5, 6);
+console.log(miArray);
+
+//NIVEL 2
+
+semana.indexOf("martes");
+
+let emojis = ["🥰​​","​🥹​​","😒​"];
+emojis.join('')
+
+
+
