4563c270467c22fc1ed948edf9453d94ee0cd488
AndreaDom3 <andreadominguez136@gmail.com>
2025-11-27 18:56:39 +0100

    dia28b

--- dia28/js/main.js
+++ dia28/js/main.js
@@ -15,20 +15,36 @@ btns.forEach(btn => btn.addEventListener("click", bgChange))
 
 
 //NIVEL 2.2
-        function generarColorAleatorio() {
-            const letrasHex = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letrasHex[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+function generarColorAleatorio() {
+  const letrasHex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letrasHex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-        function cambiarColores() {
+function cambiarColores() {
 
-            const divs = document.querySelectorAll('.color');
+  const divs = document.querySelectorAll('.color');
 
-            divs.forEach(div => {
-                div.style.backgroundColor = generarColorAleatorio();
-            });
-        }
+  divs.forEach(div => {
+    div.style.backgroundColor = generarColorAleatorio();
+  });
+}
+
+//NIVEL 2.4
+function getRandomHexColor() {
+  const randomColor = () => `#${Math.random().toString(16).slice(2, 8).padEnd(6, '0')}`;
+  return randomColor();
+}
+
+const colorBox = document.getElementById('colorBox');
+
+colorBox.addEventListener('mouseenter', function () {
+  colorBox.style.backgroundColor = getRandomHexColor();
+});
+
+colorBox.addEventListener('mouseleave', function () {
+  colorBox.style.backgroundColor = getRandomHexColor();
+});
